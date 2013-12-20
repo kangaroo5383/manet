6908e3d4c0ab76b49674d96f2c8eac2ed20e9394
Chris Prucha <chris@sugarbox.io>
2013-12-19 17:30:39 -0800

    Initial CSS selection utilities, build system, etc

---
+++ app/js/selection.js
@@ -0,0 +1,62 @@
+angular.module("util").
+factory("selection", function() {
+
+	return {
+		generateSelectorArrayForNode: function(node, selectorArray) {
+			selectorArray = selectorArray || [];
+			var parent = node && node.parentElement;
+
+			if (parent) {
+				var nodeIndexInParent = Array.prototype.indexOf.call(parent.children, node);
+				selectorArray.unshift(":nth-child(" + (nodeIndexInParent + 1) + ") ");
+				selectorArray.unshift("> *");
+
+				return this.generateSelectorArrayForNode(parent, selectorArray);
+			}
+			selectorArray.unshift("html ");
+			return selectorArray;
+		},
+		selectorArrayToString: function(selectorArray) {
+			return selectorArray.join("").trim();
+		},
+		findMatchingSelectorForNodes: function(nodes) {
+			var selectorArray;
+			nodes.forEach(function(node) {
+				var selector = this.generateSelectorArrayForNode(node);
+				if (!selectorArray) {
+					selectorArray = selector;
+				} else {
+					for (var i = 0; i < selectorArray.length; i += 1) {
+						if (selectorArray[i] !== selector[i]) {
+							selectorArray.splice(i, selectorArray.length);
+							break;
+						}
+					}
+				}
+			});
+			return selectorArray;
+		},
+		generateClassSelectorFromNode: function(node) {
+			var classes = node.getAttribute("class");
+			if (classes) {
+				classes = classes.split(" ");
+				return "." + classes.join(".");
+			}
+			return null;
+		}
+	};
+	
+});
+
+// Alogorithm notes
+// html body *:nth-child(1) *:nth-child(1) *:nth-child(1) *:nth-child(1) *:nth-child(1) //.blahBorder
+
+// html body *:nth-child(1) *:nth-child(1) *:nth-child(2) *:nth-child(2) *:nth-child(1) //.blahBorder
+
+// html body *:nth-child(1) *:nth-child(1) *:nth-child(4) *:nth-child(3) *:nth-child(1) //.blahBorder
+
+
+// html body div table:nth-child(1) tdbody tr:nth-child(1) td:nth-child(1) div:nth-child(1) h1:nth-child(1)
+// html body div table:nth-child(1) tdbody tr:nth-child(1) td:nth-child(1) div:nth-child(1) h1:nth-child(1)
+// html body div table:nth-child(1) tdbody tr:nth-child(1) td:nth-child(1) div:nth-child(1) h1:nth-child(1)
+// html body div table:nth-child(1) tdbody tr:nth-child(1) td:nth-child(1) div:nth-child(1) h1:nth-child(1)
---
+++ config.coffee
@@ -0,0 +1,39 @@
+exports.config =
+  # See docs at http://brunch.readthedocs.org/en/latest/config.html.
+  sourceMaps: false
+  conventions:
+    assets: /^app\/assets\//
+  modules:
+    definition: false
+    wrapper: false
+  paths:
+    public: '_public'
+  files:
+    javascripts:
+      joinTo:
+        'js/app.js': /^(bower_components|vendor|app)\/(?!bootstrap)/
+      order:
+        before: [
+          # All vendor components should be built first in the order they are used.
+        ]
+
+    stylesheets:
+      joinTo:
+        'css/app.css': /^(app|vendor|bower_components)/
+      order:
+        before: [
+        ]
+
+    templates:
+      joinTo: 
+        'js/dontUseMe' : /^app/ # dirty hack for Jade compiling.
+
+  plugins:
+     uglify:
+      mangle: true
+      compress:
+        global_defs: 
+          DEV: false
+
+  # Enable or disable minifying of result js / css files.
+  # minify: true
