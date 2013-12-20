b3df912ca59df1ee1aab0bfe5a3fb10d851461cf
Simon Last <stlast1@gmail.com>
2013-12-19 17:58:27 -0800

    ui changes.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,13 @@
 (function() {
-	var scriptToInject = "//localhost/bootstrap.js";
-	var newScript = document.createElement("script");
+	var scriptToInject = '//localhost:9999/ui.js';
+	var newScript = document.createElement('script');
 	newScript.src = scriptToInject;
 	document.head.appendChild(newScript);
+
+  var cssToInject = '//localhost:9999/manet.css';
+  var link = document.createElement('link');
+  link.rel = "stylesheet";
+  link.type = "text/css";
+  link.href = cssToInject;
+  document.head.appendChild(link);
 }());
