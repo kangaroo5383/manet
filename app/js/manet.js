a550fb047c8951eb93750ea371e5218211a4b0d2
Chris Prucha <chris@sugarbox.io>
2013-12-20 11:09:37 -0800

    Sample pattern highlighting.

--- app/js/manet.js
+++ app/js/manet.js
@@ -26,26 +26,10 @@ run(function(selection, Hoverbox) {
 
 	var createHoverboxFromNode = function(node) {
 		var hoverbox = document.createElement("div");
-		var matches;
 		hoverbox.classList.add("hoverbox");
 		setHoverboxNode(hoverbox, node);
 		hoverbox.innerHTML = hoverboxHtml;
 		setupHoverboxEvents(hoverbox);
-		model.selections.push(node);
-
-		// Just a test to see if this works
-		if (model.selections.length > 1) {
-			console.log(selection.generateClassSelectorFromNode(node));
-			matches = document.querySelectorAll(
-				selection.selectorArrayToString(selection.findMatchingSelectorForNodes(model.selections))
-			);
-		} else {
-			matches = document.querySelector(
-				selection.selectorArrayToString(selection.generateSelectorArrayForNode(node))
-			);
-		}
-
-		console.log(matches)
 
 
 		// hoverbox.querySelector("input").addEventListener("input", function() {
@@ -106,12 +90,33 @@ run(function(selection, Hoverbox) {
 		currentHoverbox.hoverbox.style.display = "none";
 	};
 
-
+	var addHoverboxesForNodeMatches = function(node) {
+		var matches;
+		model.selections.push(node);
+		// Just a test to see if this works
+		if (model.selections.length > 1) {
+			matches = document.querySelectorAll(
+				selection.selectorArrayToString(selection.findMatchingSelectorForNodes(model.selections))
+			);
+			Array.prototype.forEach.call(matches, function(match) {
+				var hoverbox = createHoverboxFromNode(match);
+				hoverbox.style.outline = "2px dotted grey";
+				hoverbox.style.background = "rgba(22,22,22,0.2)";
+				uiContainer.querySelector(".current").appendChild(hoverbox);
+			});
+		} else {
+			matches = document.querySelector(
+				selection.selectorArrayToString(selection.generateSelectorArrayForNode(node))
+			);
+		}
+	}
 
 	var addCurrentHoverbox = function(e) {
 		e.preventDefault();
 		var target = e.target;
 		if (targetIsValid(target)) {
+
+			addHoverboxesForNodeMatches(target);
 			var hoverbox = createHoverboxFromNode(target);
 			uiContainer.querySelector(".current").appendChild(hoverbox);
 		}
