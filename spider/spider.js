5a99589ada1918103933672f3300b5c2b1361a7a
Jessica Lam <jessica.l4m@gmail.com>
2013-12-21 03:35:58 -0800

    Added test and refactored spider.

--- spider/spider.js
+++ spider/spider.js
@@ -1,43 +1,22 @@
 // var dataCollector = (domainUrl, filterUrlRegex, transformJson);
 
 var transformer = require('../applyTransform/transformer.js');
+var fs = require('fs');
+var url = require('url');
+var Crawler = require("crawler").Crawler;
+var phantom = require('node-phantom');
 
 var domainToCrawl, topLevelDomainToCrawl;
-var crawlCount = 1;
 
-function getHostname(url) {
-    var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
-	var domain = matches && matches[1];
-	return domain;
-}
-
-function getTopLevelDomain(url){
-	var topLevelDomain = null;
-	var hostname = getHostname(url);
-	
-	if(hostname != null){	
-		// we handle the case where no hostname exists in the URL, 
-		// e.g. if it's not a properly formed href, or it's javascript or something 			
-		var hostnameParts = hostname.split('.');
-		
-		// the top level domain is the last two parts of the hostname (i.e. 
-		// we want something like rottentomatoes.com 
-		// regardless of if it's dev.rottentomatoes.com, or www.rottentomatoes.com etc.
-		topLevelDomain = hostnameParts.slice(hostnameParts.length - 2).join('.');
-	}
-	
-	return topLevelDomain;
-}
 
 function spider () {};
 
-var transform = function (content, transformerDictionary) {
-		transformer.transformContentWithTransformerDictionary(content, {
-		characters : ".characters",
-		rating : "#all-critics-meter"
-	}, function(err, resultDictionary) {
+var transform = function (aURL, content, transformerDictionary) {
+	console.log("transforming");
+	transformer.transformURLWithTransformerDictionary(aURL, transformerDictionary, function(err, resultDictionary) {
+		console.log("handled" + aURL);
 		if (err) {
-			console.log("some error:" + err.message);
+			console.log("transform error:" + err.message);
 		} else {
 			if (resultDictionary && resultDictionary !== null) {
 				console.log("HI");
@@ -48,74 +27,47 @@ var transform = function (content, transformerDictionary) {
 		}
 	});
 }
-// Call this with e.g. domainToCrawl = 'http://rottentomatoes.com';
-spider.run = function(domainToCrawl){
 
-	
+var shouldQueue = function (currentDomain, aURL) {
+	return (currentDomain === url.parse(aURL).hostname);
+};
+
+// Call this with e.g. domainToCrawl = 'http://rottentomatoes.com';
+spider.run = function(baseURL, transformerDictionary){
+	var crawlCount = 1;
+	var currentDomain = url.parse(baseURL).hostname;
 	var mongo = require('mongoskin');
 	var db = mongo.db('localhost:27017/manet');
 	
-	var Crawler = require("crawler").Crawler;
 	
 	var c = new Crawler({
 		"maxConnections":10,
+		"skipDuplicates": true,
 		// This will be called for each crawled page
 		"callback":function(error,result,$) {
-
-			console.log("queued: " + result.uri);
-			transform(result.body, {});
-
-			$('a').each(function(index){
-				// We don't want to follow links that are not on the same top level domain
-				// or ones that have things like javascript(0) in their href, so we
-				// select only links that have an href that starts with a '/' and has
-				// some text after - we exclude just the / since that's the starting point
-				// we feed in.			
-				var href = $(this).prop('href').replace('www.','');
-				
-				var topLevelDomain = getTopLevelDomain(href); 
-				
-			
-				if(topLevelDomain == topLevelDomainToCrawl){
-	
-					db.collection('urls').findById(href, function(err, result){
-						console.log(result);
-						if(result == null){
-							console.log("Crawling " + href);
-							crawlCount++;
-							console.log("Crawl Count: " + crawlCount);
-							db.collection('urls').insert({_id: href, crawl: 1}, {}, function(err,result){
-								//console.log(result);
-								if(err == null){
-									console.log(href + " was registered ");
-									console.log("Applying transformation doc");
-									
-								}
-								
-							});
-		 					c.queue(href);
-		 				}
-						else{
-							console.log('Already crawled ' + href);
-						}						
-					});
-						
-				}
-				
+			if (error) {
+				console.log("crawling error");
+			}
+			var currentURI = result.uri.replace("http://www.", "http://");
+			currentURI = currentURI.replace("http://", "http://www.");
+			console.log(currentURI + " was registered ");
+			console.log("Applying transformation doc");
+			// transform(currentURI, result.body, transformerDictionary);
+			transformer.test(currentURI, transformerDictionary, function (err, dictionary){
+				console.log("returned");
+				console.log(dictionary);
 			});
-			
+				
+			$('a').each(function(index, el){
+				var href = $(el).prop('href');				
+				if(href && href !== "" && shouldQueue(currentDomain, href)){
+					c.queue(href);						
+				}
+			});
 		}
 	});
 
-	topLevelDomainToCrawl = getTopLevelDomain(domainToCrawl); 
-	
-	db.collection('urls').insert({_id: domainToCrawl, crawl: '1'}, function(err,result){
-		console.log(result);
-		console.log(domainToCrawl + " was registered ");
-	});
-
-	c.queue(domainToCrawl);
-
+	c.queue(baseURL);
 };
 
 
--- spider/test.js
+++ spider/test.js
@@ -1,3 +1,26 @@
 var spider = require('./spider.js');
-var testURL = "http://rottentomatoes.com";
-spider.run(testURL);
+var testURL = "http://rottentomatoes.com/";
+
+var response = {
+	url: "http://www.rottentomatoes.com/",
+	transformer: [{
+		key: "comments",
+		selector: "html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P"
+	},
+	{
+		key: "comments2",
+		selector: "html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P"
+	}]};
+
+var processInput = function (inputDictionary) {
+	var transformer = inputDictionary.transformer;
+	var result = {};
+	for (var aKey in transformer) {
+		result[transformer[aKey]["key"]] = transformer[aKey]["selector"];
+	}
+	console.log(result);
+	return result;
+};
+
+var transformerDictionary = processInput(response);
+spider.run(testURL, transformerDictionary);
