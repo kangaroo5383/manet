c9638ff779ca05a2fcc14c95343dbe3a6af39a58
Jessica Lam <jessica.l4m@gmail.com>
2013-12-21 04:22:14 -0800

    New selector generation algorithm now works without phantomjs.  Removed.

--- applyTransform/test.js
+++ applyTransform/test.js
@@ -4,8 +4,8 @@ var transformer = require('./transformer.js');
 var aURL = "http://www.rottentomatoes.com/m/anchorman_2_the_legend_continues/";
 
 var transformerFile = "/Users/jessicalam/Development/manet/applyTransform/transformer-sample.json";
-var sampleTransformDictionary = { comments: 'html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P',
-  comments2: 'html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P' };
+var sampleTransformDictionary = { comments: 'html DIV.body_main DIV.col#mainColumn DIV#mobPanel DIV.content DIV.content_box#contentReviews DIV.content_body DIV#reviews DIV.quote_bubble DIV.review_quote DIV.quote_contents DIV.media_block_content',
+  characters: 'html DIV.body_main DIV.col#mainColumn DIV#mobPanel DIV.content DIV.clearfix DIV.col_p DIV.content_box#cast-info DIV.content_body DIV.moreContainer DIV.more-list UL.clearfix LI.media_block DIV.media_block_content SPAN.characters' };
 
 var transform2 = {
 	"characters" : ".characters",
@@ -31,27 +31,27 @@ var transform2 = {
 // 	}
 // });
 
-transformer.test(aURL, sampleTransformDictionary, function(err, resultDictionary) {
-	if (err || !resultDictionary) {
-		console.log("no result:");
-	} else {
-		console.log(JSON.stringify(resultDictionary));	
-		console.log("hi");
-	}
-});
-
-
-// var testHTML = fs.readFileSync("/Users/jessicalam/Desktop/test2.html", 'utf8');
-// console.log("3rd");
-// transformer.transformContentWithTransformerDictionary(testHTML, sampleTransformDictionary, function(err, resultDictionary) {
-// 	if (err) {
-// 		console.log("err");
+// transformer.test(aURL, sampleTransformDictionary, function(err, resultDictionary) {
+// 	if (err || !resultDictionary) {
+// 		console.log("no result:");
 // 	} else {
-// 		if (resultDictionary && resultDictionary != null)  {
-// 			console.log(resultDictionary);
-// 		}
-// 		else {
-// 			console.log("no data");
-// 		}
+// 		console.log(JSON.stringify(resultDictionary));	
+// 		console.log("hi");
 // 	}
 // });
+
+
+var testHTML = fs.readFileSync("/Users/jessicalam/Desktop/test2.html", 'utf8');
+console.log("3rd");
+transformer.transformContentWithTransformerDictionary(testHTML, sampleTransformDictionary, function(err, resultDictionary) {
+	if (err) {
+		console.log("err");
+	} else {
+		if (resultDictionary && resultDictionary != null)  {
+			console.log(resultDictionary);
+		}
+		else {
+			console.log("no data");
+		}
+	}
+});
--- applyTransform/transformer.js
+++ applyTransform/transformer.js
@@ -63,48 +63,6 @@ var processContentAtURLWithTransformerFileURL = function (aURL, transformerDicti
   });  
 }
 
-var test = function (aURL, transformerDictionary) {
-  var that = this;
-  jsPhantom.create(function(err,ph) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("hi");
-    }
-    return ph.createPage(function(err,page) {
-      return page.open(aURL, function(err,status) {
-        console.log("opened site? ", status);
-        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function(err) {
-          if(err) {
-            console.log(err);
-          }
-          //jQuery Loaded.
-        //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
-        setTimeout(function() {
-          return page.evaluate(function() {
-            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
-            var h2Arr = [],
-            pArr = [];
-
-
-            $('html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child()').each(function() {
-              pArr.push($(this).html());
-            });
-
-            return {
-              h2: h2Arr,
-              p: pArr
-            };
-          }, function(err,result) {
-            console.log(result);
-            ph.exit();
-          });
-        }, 5000);
-        });
-      });
-    });
-  });
-}
 function transformer() {}
 
 transformer.transformURLWithTransformerFile = function (aURL, transformerFile, callback) {
@@ -129,9 +87,6 @@ transformer.transformContentWithTransformerDictionary = function (content, trans
   transformContentWithTransformerDictionary(content, transformerDictionary, callback);
 };
 
-transformer.test = function (aURL, transformerDictionary, callback) {
-  test(aURL, transformerDictionary, callback);
-}
 module.exports = transformer;
 
 
