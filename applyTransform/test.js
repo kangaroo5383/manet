036b2118346e21b98d4ce2e91ea3bd118836a219
Jessica Lam <jessica.l4m@gmail.com>
2013-12-21 03:35:31 -0800

    Added phantom for testing.

--- applyTransform/test.js
+++ applyTransform/test.js
@@ -1,24 +1,57 @@
+var fs = require('fs');
 var transformer = require('./transformer.js');
-var aURL = "http://www.rottentomatoes.com/m/the_hunger_games_catching_fire/";
-// var aURL = "http://rottentomatoes.com/m/anchorman_2_the_legend_continues/";
+// var aURL = "http://www.rottentomatoes.com/m/the_hunger_games_catching_fire/";
+var aURL = "http://www.rottentomatoes.com/m/anchorman_2_the_legend_continues/";
 
 var transformerFile = "/Users/jessicalam/Development/manet/applyTransform/transformer-sample.json";
+var sampleTransformDictionary = { comments: 'html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P',
+  comments2: 'html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child(2) P' };
 
-transformer.transformURLWithTransformerFile(aURL, transformerFile, function(err, resultDictionary) {
+var transform2 = {
+	"characters" : ".characters",
+	"rating" : "#all-critics-meter"
+};
+
+// console.log("1st");
+// transformer.transformURLWithTransformerFile(aURL, transformerFile, function(err, resultDictionary) {
+// 	if (err || !resultDictionary) {
+// 		console.log("no result:");
+// 	} else {
+// 		console.log(JSON.stringify(resultDictionary));	
+// 	}
+// });
+
+// console.log("2st");
+// transformer.transformURLWithTransformerDictionary(aURL, sampleTransformDictionary, function(err, resultDictionary) {
+// 	if (err || !resultDictionary) {
+// 		console.log("no result:");
+// 	} else {
+// 		console.log(JSON.stringify(resultDictionary));	
+// 		console.log("hi");
+// 	}
+// });
+
+transformer.test(aURL, sampleTransformDictionary, function(err, resultDictionary) {
 	if (err || !resultDictionary) {
 		console.log("no result:");
 	} else {
 		console.log(JSON.stringify(resultDictionary));	
+		console.log("hi");
 	}
 });
 
-transformer.transformURLWithTransformerDictionary(aURL, {
-	characters : ".characters",
-	rating : "#all-critics-meter"
-}, function(err, resultDictionary) {
-	if (err || !resultDictionary) {
-		console.log("no result:");
-	} else {
-		console.log(JSON.stringify(resultDictionary));	
-	}
-});
+
+// var testHTML = fs.readFileSync("/Users/jessicalam/Desktop/test2.html", 'utf8');
+// console.log("3rd");
+// transformer.transformContentWithTransformerDictionary(testHTML, sampleTransformDictionary, function(err, resultDictionary) {
+// 	if (err) {
+// 		console.log("err");
+// 	} else {
+// 		if (resultDictionary && resultDictionary != null)  {
+// 			console.log(resultDictionary);
+// 		}
+// 		else {
+// 			console.log("no data");
+// 		}
+// 	}
+// });
--- applyTransform/transformer.js
+++ applyTransform/transformer.js
@@ -8,6 +8,7 @@ var myWindow = jsdom().createWindow();
 var jQuery = require('jquery').create(myWindow);
 
 var format = require('util').format;
+var jsPhantom = require('node-phantom');
 
 var valueForSelectorString = function (selectorString) {
   var output = [];
@@ -17,11 +18,14 @@ var valueForSelectorString = function (selectorString) {
   return output;
 };
 var transformContentWithTransformerDictionary = function (content, transformerDictionary, callback) {
+  myWindow.document.innerHTML = content;
   var dictionary = {};
   var valid = false;
   var transformerKeys = Object.keys(transformerDictionary);
   for (var i in transformerKeys) {
+
     var aKey = transformerKeys[i];
+    console.log(aKey);
     var selectedValue = valueForSelectorString(transformerDictionary[aKey]);
     if (selectedValue.length > 0) {
       valid = true;
@@ -42,6 +46,7 @@ var processContentAtURLWithTransformerFileURL = function (aURL, transformerDicti
   
   http.get(aURL, function(response) {
     if (response.statusCode != 200) {
+      console.log("status: " + response.statusCode);
       callback({message:"file not accessible via http"}, null);
     }
     response.setEncoding('utf8');
@@ -49,10 +54,8 @@ var processContentAtURLWithTransformerFileURL = function (aURL, transformerDicti
       content = content + chunk;
     });
     response.on("end", function(){
-      myWindow.document.innerHTML = content;
-      content = "";
-      
       transformContentWithTransformerDictionary(content, transformerDictionary, callback);
+      content = "";
     });
   }).on('error', function(e) {
     console.log("Got error: " + e.message);
@@ -60,6 +63,48 @@ var processContentAtURLWithTransformerFileURL = function (aURL, transformerDicti
   });  
 }
 
+var test = function (aURL, transformerDictionary) {
+  var that = this;
+  jsPhantom.create(function(err,ph) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("hi");
+    }
+    return ph.createPage(function(err,page) {
+      return page.open(aURL, function(err,status) {
+        console.log("opened site? ", status);
+        page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function(err) {
+          if(err) {
+            console.log(err);
+          }
+          //jQuery Loaded.
+        //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+        setTimeout(function() {
+          return page.evaluate(function() {
+            //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+            var h2Arr = [],
+            pArr = [];
+
+
+            $('html > *:nth-child(2) > *:nth-child(14) > *:nth-child(2) > *:nth-child(1) > *:nth-child(1) > *:nth-child(10) > *:nth-child(2) > *:nth-child()').each(function() {
+              pArr.push($(this).html());
+            });
+
+            return {
+              h2: h2Arr,
+              p: pArr
+            };
+          }, function(err,result) {
+            console.log(result);
+            ph.exit();
+          });
+        }, 5000);
+        });
+      });
+    });
+  });
+}
 function transformer() {}
 
 transformer.transformURLWithTransformerFile = function (aURL, transformerFile, callback) {
@@ -76,13 +121,17 @@ transformer.transformURLWithTransformerDictionary = function (aURL, transformerD
   if (aURL && aURL !== "" && transformerDictionary && transformerDictionary !== "") {
     processContentAtURLWithTransformerFileURL(aURL, transformerDictionary, callback);
   } else {
-    console.log("missing parameter in transformURLWithTransformerFile");
+    console.log("missing parameter in transformURLWithTransformerDictionary");
   }
 };
 
 transformer.transformContentWithTransformerDictionary = function (content, transformerDictionary, callback) {
   transformContentWithTransformerDictionary(content, transformerDictionary, callback);
 };
+
+transformer.test = function (aURL, transformerDictionary, callback) {
+  test(aURL, transformerDictionary, callback);
+}
 module.exports = transformer;
 
 
